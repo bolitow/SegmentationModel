7c62a03664273d8bdaf58e93fae43f7544f02d30
Bolito <92942145+bolitow@users.noreply.github.com>
2025-06-26 09:17:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Start FastAPI backend on $PORT
-cd /app/api && uvicorn main:app --host 0.0.0.0 --port $PORT &
+cd /app/api && uvicorn main:app --host 0.0.0.0 --port $PORT
 
-# Start Streamlit frontend on port 8501
-cd /app/app && streamlit run app.py --server.port 8501 --server.address 0.0.0.0 &
-
-# Wait for any process to exit
-wait -n
-
-# Exit with status of process that exited first
+# Exit with status of process
 exit $?
